0489daeb56318e01101278eaeeebe1c664685470
SumRandomPerson <113185567+SumRandomPerson@users.noreply.github.com>
2023-10-16 12:30:36 -0400

    changes

--- Assets/Course Library/Scripts/PlayerControler.cs
+++ Assets/Course Library/Scripts/PlayerControler.cs
@@ -8,6 +8,7 @@ public class PlayerControler : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +30,11 @@ public class PlayerControler : MonoBehaviour
 
        horizontalInput = Input.GetAxis("Horizontal");
        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+       }
+    
     }
 }
